96548dfcfccf7b0b12fd8a9a82c67428b94f7e7b
osgilliath <shankhdhar.madhav@gmail.com>
2025-11-05 20:02:51 +0530

    fixed audio errors

--- yt-bay.py
+++ yt-bay.py
@@ -1,19 +1,22 @@
+
 import os
+import imageio_ffmpeg
 
 def download_media():
+    ffmpeg_location = imageio_ffmpeg.get_ffmpeg_exe()
     url = input("Enter the YouTube URL: ")
-    choice = input("Download as (audio/video): ").lower()
+    format = input("Download as (mp3/mp4): ").lower()
 
-    if choice == 'video':
-        print("Downloading video...")
-        os.system(f'yt-dlp -f best {url}')
-        print("Video download completed.")
-    elif choice == 'audio':
-        print("Downloading audio...")
-        os.system(f'yt-dlp -x --audio-format best {url}')
-        print("Audio download completed.")
+    if format == 'mp4':
+        print("Downloading and converting to mp4...")
+        os.system(f'yt-dlp --ffmpeg-location "{ffmpeg_location}" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" --merge-output-format mp4 {url}')
+        print("Download and conversion to mp4 completed.")
+    elif format == 'mp3':
+        print("Downloading and converting to mp3...")
+        os.system(f'yt-dlp --ffmpeg-location "{ffmpeg_location}" -x --audio-format mp3 {url}')
+        print("Download and conversion to mp3 completed.")
     else:
-        print("Invalid choice. Please choose 'audio' or 'video'.")
+        print("Invalid format. Please choose 'mp3' or 'mp4'.")
 
 if __name__ == "__main__":
     download_media()
