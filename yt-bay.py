c824a8ae979f6aaea000cfa8c6420fda7fa983e3
osgilliath <shankhdhar.madhav@gmail.com>
2025-11-05 19:53:19 +0530

    added gitignore and corrected errors

--- yt-bay.py
+++ yt-bay.py
@@ -1,15 +1,19 @@
 import os
 
-def download_video():
+def download_media():
     url = input("Enter the YouTube URL: ")
-    format = input("Download as (mp3/mp4): ").lower()
+    choice = input("Download as (audio/video): ").lower()
 
-    if format == 'mp4':
-        os.system(f'yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" {url}')
-    elif format == 'mp3':
-        os.system(f'yt-dlp -x --audio-format mp3 {url}')
+    if choice == 'video':
+        print("Downloading video...")
+        os.system(f'yt-dlp -f best {url}')
+        print("Video download completed.")
+    elif choice == 'audio':
+        print("Downloading audio...")
+        os.system(f'yt-dlp -x --audio-format best {url}')
+        print("Audio download completed.")
     else:
-        print("Invalid format. Please choose 'mp3' or 'mp4'.")
+        print("Invalid choice. Please choose 'audio' or 'video'.")
 
 if __name__ == "__main__":
-    download_video()
+    download_media()
